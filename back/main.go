4b970c999b7079f97c3ea150834836d72a54d3a8
snoy <Stav.Noy@epitech.eu>
2020-03-17 12:27:39 +0100

    Step 1

--- back/main.go
+++ back/main.go
@@ -4,24 +4,30 @@ import (
 	"fmt"
 	"github.com/gorilla/websocket"
 	"net/http"
+	"os"
+	"strings"
 )
 
 func main() {
+	dir, _ := os.Getwd()
+
 	upgrader := websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024}
 
 	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
 		conn, _ := upgrader.Upgrade(w, r, nil)
 
 		for {
-			msgType, msg, err := conn.ReadMessage()
+			msgType, input, err := conn.ReadMessage()
 			if err != nil {
 				fmt.Printf(err.Error())
 
 				return
 			}
 
+			output := []byte(strings.ToUpper(string(input)))
+
 			// Write message back to browser
-			if err = conn.WriteMessage(msgType, msg); err != nil {
+			if err = conn.WriteMessage(msgType, output); err != nil {
 				fmt.Printf(err.Error())
 
 				return
@@ -30,7 +36,7 @@ func main() {
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello\n")
+		http.ServeFile(w, r, dir + "/front/simple.html")
 	})
 
 	http.ListenAndServe(":8080", nil)
